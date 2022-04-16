21db0f7c0136cfe5fb037a1bf43207be52589ffd
pawan36475 <82301521+pawan36475@users.noreply.github.com>
2022-04-16 17:26:55 +0530

    Create Floor.java

---
+++ Section_06/Carpet Cost Calculator/Floor.java
@@ -0,0 +1,22 @@
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        if (width < 0)
+            this.width = 0;
+        if (length < 0)
+            this.length = 0;
+
+        this.length = length;
+        this.width = width;
+
+    }
+
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+
+
+}
