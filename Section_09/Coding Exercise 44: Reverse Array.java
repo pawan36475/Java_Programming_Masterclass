048c26ef7fc5caef7b78823eb2db0c7c274543be
Pawan Deep Singh <82301521+pawan36475@users.noreply.github.com>
2023-07-09 23:47:04 +0530

    Update Coding Exercise 44: Reverse Array.java

--- Section_09/Coding Exercise 44: Reverse Array.java
+++ Section_09/Coding Exercise 44: Reverse Array.java
@@ -1,1 +1,25 @@
+package com.JavaProgrammingMasterClass.demo;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    private static void reverse(int[] array) {
+        System.out.println("Array = " + Arrays.toString(array));
+        int maxIndex = array.length - 1;
+        int[] reversedArray = new int[array.length];
+        for (int it : array) {
+            reversedArray[maxIndex--] = it;
+        }
+        System.out.println("Reversed array = " + Arrays.toString(reversedArray));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] ar = {1, 2, 3, 4, 5};
+        reverse(ar);
+    }
+}
 
