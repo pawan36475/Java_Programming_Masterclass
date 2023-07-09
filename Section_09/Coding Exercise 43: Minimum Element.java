00709d02285b380782b379065f290e6a2c00748e
Pawan Deep Singh <82301521+pawan36475@users.noreply.github.com>
2023-07-09 21:34:56 +0530

    Update Coding Exercise 43: Minimum Element.java

--- Section_09/Coding Exercise 43: Minimum Element.java
+++ Section_09/Coding Exercise 43: Minimum Element.java
@@ -1,1 +1,42 @@
+package com.JavaProgrammingMasterClass.demo;
+
+import java.util.*;
+
+public class MinimumElement {
+
+    public static int readInteger() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the no. of elements");
+        int size = scanner.nextInt();
+
+        return size;
+    }
+
+    public static int[] readElements(int size) {
+        Scanner scanner = new Scanner(System.in);
+        int[] enteredElements = new int[size];
+        for (int i = 0; i < size; i++) {
+            enteredElements[i] = scanner.nextInt();
+        }
+        scanner.close();
+        return enteredElements;
+    }
+
+    public static int findMin(int[] array) {
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int myIntegerArray = readInteger();
+        int[] enteredElements = readElements(myIntegerArray);
+        int min = findMin(enteredElements);
+        System.out.println("Minimum element is " + min);
+    }
+}
 
